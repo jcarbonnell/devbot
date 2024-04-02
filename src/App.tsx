4d29277d5332891605e4ecabbee40ca958e6ad77
juliencarbonnell <julien.carbonnell@gmail.com>
2024-04-02 16:18:25 -0500

    original commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useAuth } from "near-social-bridge";
+import TodoList from "./components/TodoList";
+import NotLoggedInWarning from "./components/NotLoggedInWarning";
 import './App.css';
 
 function App() {
+  const auth = useAuth();
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Web3 - Todo App</h3>
+      {auth.user ? <TodoList /> : <NotLoggedInWarning />}
     </div>
   );
 }
---
+++ src/components/NewTodoForm.tsx
@@ -0,0 +1,29 @@
+// src/components/NewTodoForm.tsx
+import React from 'react';
+
+type Props = {
+    taskDescription: string;
+    onInputChange: (inputValue:string) => void;
+    onAddClick: () => void;
+};
+
+const NewTodoForm: React.FC<Props> = ({
+    taskDescription,
+    onInputChange,
+    onAddClick,
+}) => (
+    <div style={{ marginTop: "52px" }}>
+        <label style={{ marginRight: "8px" }}>Add  new task:</label>
+        <input
+            style={{ marginRight: "8px" }}
+            placeholder="Task Description"
+            value={taskDescription}
+            onChange={(e) => onInputChange(e.target.value)}
+        />
+        <button disabled={!taskDescription} onClick={onAddClick}>
+            Add
+        </button>
+    </div>
+);
+
+export default NewTodoForm;
