8a13e614253f03dbe1b50109a17fd724b8b8dd45
juliencarbonnell <julien.carbonnell@gmail.com>
2024-04-12 09:16:48 -0500

    update bos widget + npm frontend

--- bos/RepoToText.jsx
+++ bos/RepoToText.jsx
@@ -1,9 +1,6 @@
 // bos/RepoToText.jsx
-const externalAppUrl = 'https://devbot.hellopartage.xyz:5000'
-
 return (
     <Widget
-        src={'wendersonpires.near/widget/NearSocialBridgeCore'}
-        props={{ externalAppUrl }}
+        src={'jcarbonnell.near/widget/GithubScraper'}
     />
 )
